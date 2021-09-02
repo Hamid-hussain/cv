313f488a89eeeacb017028fa4251a93c68fc166b
Hamidoo <hussainhafeez1100@gmail.com>
2021-09-02 14:00:28 +0530

    added all files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      animation: ['responsive', 'motion-safe', 'motion-reduce'],
+      animation: ['hover', 'focus'],
+    },
+  },
+  plugins: [],
+}
